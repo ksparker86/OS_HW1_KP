50832966e351248aef7f1b94091aa55c8b3f720f
ksparker86 <ksparker@mymail.mines.edu>
2021-09-20 17:09:43 -0600

    First Commit

---
+++ multiboot_header.asm
@@ -0,0 +1,17 @@
+section .multiboot_header
+magic equ 0xe85250d6
+arch equ 0
+; multiboot 2
+; protected mode i386
+header_start:
+dd magic
+; magic number
+dd arch
+; architecture
+dd header_end - header_start
+; header length
+dd 0x100000000 - (magic + arch + (header_end - header_start))
+dw 0 ; type
+dw 0 ; flags
+dd 8 ; size
+header_end:
